9fad2f14944fb3072bccd5c351caf4480d6f43fb
ccarlos <2238513100@qq.com>
2020-12-05 22:05:15 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第1周   万丈高楼，地基首要 2-10 聚合工程整合SpringBoot

---
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.ccarlosf.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Object hello() {
+        return "Hello World~";
+    }
+
+}
