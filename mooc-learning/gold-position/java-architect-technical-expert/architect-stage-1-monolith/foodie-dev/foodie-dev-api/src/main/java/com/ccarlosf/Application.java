f3e88e9cbc42d021585329bafdf7c997ec21fdc4
ccarlos <2238513100@qq.com>
2020-12-08 22:42:23 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第1周   万丈高楼，地基首要 4-4 用户注册 - 创建用户service

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/Application.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/Application.java
@@ -3,11 +3,14 @@ package com.ccarlosf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 // 扫描 mybatis 通用 mapper 所在的包
 @MapperScan(basePackages = "com.ccarlosf.mapper")
+// 扫描所有包以及相关组件包
+@ComponentScan(basePackages = {"org.n3r.idworker"})
 public class Application {
 
     public static void main(String[] args) {
--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-service/src/main/java/com/ccarlosf/service/UserService.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-service/src/main/java/com/ccarlosf/service/UserService.java
@@ -1,5 +1,8 @@
 package com.ccarlosf.service;
 
+import com.ccarlosf.pojo.Users;
+import com.ccarlosf.pojo.bo.UserBO;
+
 public interface UserService {
 
     /**
@@ -7,4 +10,13 @@ public interface UserService {
      */
     public boolean queryUsernameIsExist(String username);
 
+    /**
+     * 判断用户名是否存在
+     */
+    public Users createUser(UserBO userBO);
+
+    /**
+     * 检索用户名和密码是否匹配，用于登录
+     */
+    public Users queryUserForLogin(String username, String password);
 }
