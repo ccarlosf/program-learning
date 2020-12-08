be259b35cbc163d58ffd07c53503729bb7ba9c54
ccarlos <2238513100@qq.com>
2020-12-08 23:01:53 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第1周   万丈高楼，地基首要 4-5 用户注册 - 创建用户controller与测试

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/Application.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/Application.java
@@ -10,7 +10,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 // 扫描 mybatis 通用 mapper 所在的包
 @MapperScan(basePackages = "com.ccarlosf.mapper")
 // 扫描所有包以及相关组件包
-@ComponentScan(basePackages = {"org.n3r.idworker"})
+@ComponentScan(basePackages = {"com.ccarlosf","org.n3r.idworker"})
 public class Application {
 
     public static void main(String[] args) {
--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/PassportController.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/PassportController.java
@@ -1,14 +1,15 @@
 package com.ccarlosf.controller;
 
+import com.ccarlosf.pojo.Users;
+import com.ccarlosf.pojo.bo.UserBO;
 import com.ccarlosf.service.UserService;
 import com.ccarlosf.utils.JSONResult;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("passport")
@@ -35,5 +36,42 @@ public class PassportController {
         return JSONResult.ok();
     }
 
+    @PostMapping("/regist")
+    public JSONResult regist(@RequestBody UserBO userBO,
+                             HttpServletRequest request,
+                             HttpServletResponse response) {
+
+        String username = userBO.getUsername();
+        String password = userBO.getPassword();
+        String confirmPwd = userBO.getConfirmPassword();
+
+        // 0. 判断用户名和密码必须不为空
+        if (StringUtils.isBlank(username) ||
+                StringUtils.isBlank(password) ||
+                StringUtils.isBlank(confirmPwd)) {
+            return JSONResult.errorMsg("用户名或密码不能为空");
+        }
+
+        // 1. 查询用户名是否存在
+        boolean isExist = userService.queryUsernameIsExist(username);
+        if (isExist) {
+            return JSONResult.errorMsg("用户名已经存在");
+        }
+
+        // 2. 密码长度不能少于6位
+        if (password.length() < 6) {
+            return JSONResult.errorMsg("密码长度不能少于6");
+        }
+
+        // 3. 判断两次密码是否一致
+        if (!password.equals(confirmPwd)) {
+            return JSONResult.errorMsg("两次密码输入不一致");
+        }
+
+        // 4. 实现注册
+        Users userResult = userService.createUser(userBO);
+
+        return JSONResult.ok();
+    }
 
 }
