e4d9697941aed3acd9fded93373fcb8dece7002a
ccarlos <2238513100@qq.com>
2020-12-13 22:24:14 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第1周   万丈高楼，地基首要 4-12 回顾cookie与session

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/HelloController.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/HelloController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @ApiIgnore
 @RestController
 public class HelloController {
@@ -13,5 +16,16 @@ public class HelloController {
     public Object hello() {
         return "Hello ";
     }
+
+    @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo", "new user");
+        session.setMaxInactiveInterval(3600);
+        session.getAttribute("userInfo");
+//        session.removeAttribute("userInfo");
+        return "ok";
+    }
+
 
 }
