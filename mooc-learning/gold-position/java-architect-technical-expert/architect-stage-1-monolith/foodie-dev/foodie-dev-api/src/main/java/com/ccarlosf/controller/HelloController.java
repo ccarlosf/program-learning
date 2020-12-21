97b1e4372f01abce818aefba964709959d7e8831
ccarlos <2238513100@qq.com>
2020-12-21 22:44:47 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第1周   万丈高楼，地基首要 4-14 整合log4j打印日志

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/HelloController.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.ccarlosf.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
@@ -12,8 +14,16 @@ import javax.servlet.http.HttpSession;
 @RestController
 public class HelloController {
 
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/hello")
     public Object hello() {
+
+        logger.debug("debug: hello~");
+        logger.info("info: hello~");
+        logger.warn("warn: hello~");
+        logger.error("error: hello~");
+
         return "Hello ";
     }
 
