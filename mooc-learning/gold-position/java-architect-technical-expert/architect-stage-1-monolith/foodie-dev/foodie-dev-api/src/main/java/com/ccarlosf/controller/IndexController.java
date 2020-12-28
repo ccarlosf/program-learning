12a2b0f3e3ee89dcd731d2fd16cc82d09f83464a
ccarlos <2238513100@qq.com>
2020-12-28 21:59:46 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第2周   分类，推荐，搜索，评价，购物车开发 1-2 首页分类需求分析

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/IndexController.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.ccarlosf.controller;
 
 import com.ccarlosf.enums.YesOrNo;
 import com.ccarlosf.pojo.Carousel;
+import com.ccarlosf.pojo.Category;
 import com.ccarlosf.service.CarouselService;
 import com.ccarlosf.utils.JSONResult;
 import io.swagger.annotations.Api;
@@ -29,6 +30,12 @@ public class IndexController {
         List<Carousel> list = carouselService.queryAll(YesOrNo.YES.type);
         return JSONResult.ok(list);
     }
+
+    /**
+     * 首页分类展示需求：
+     * 1. 第一次刷新主页查询大分类，渲染展示到首页
+     * 2. 如果鼠标上移到大分类，则加载其子分类的内容，如果已经存在子分类，则不需要加载（懒加载）
+     */
 
 
 }
