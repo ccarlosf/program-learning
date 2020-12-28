75cd54d90596b50d561e99143943869c33b56fd0
ccarlos <2238513100@qq.com>
2020-12-28 21:52:19 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第2周   分类，推荐，搜索，评价，购物车开发 1-1 实现电商首页轮播图功能

---
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/IndexController.java
@@ -0,0 +1,34 @@
+package com.ccarlosf.controller;
+
+import com.ccarlosf.enums.YesOrNo;
+import com.ccarlosf.pojo.Carousel;
+import com.ccarlosf.service.CarouselService;
+import com.ccarlosf.utils.JSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Api(value = "首页", tags = {"首页展示的相关接口"})
+@RestController
+@RequestMapping("index")
+public class IndexController {
+
+    @Autowired
+    private CarouselService carouselService;
+
+    @ApiOperation(value = "获取首页轮播图列表", notes = "获取首页轮播图列表", httpMethod = "GET")
+    @GetMapping("/carousel")
+    public JSONResult carousel() {
+        List<Carousel> list = carouselService.queryAll(YesOrNo.YES.type);
+        return JSONResult.ok(list);
+    }
+
+
+}
