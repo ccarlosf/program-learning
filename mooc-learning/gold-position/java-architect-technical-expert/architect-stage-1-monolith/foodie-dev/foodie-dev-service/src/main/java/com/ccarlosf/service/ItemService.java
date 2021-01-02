b5348069171175b5bcb0cf837bdcf5ca90d872b4
ccarlos <2238513100@qq.com>
2021-01-02 18:58:16 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第2周   分类，推荐，搜索，评价，购物车开发 2-4 搜索 - 编写商品相关查询service

---
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-service/src/main/java/com/ccarlosf/service/ItemService.java
@@ -0,0 +1,41 @@
+package com.ccarlosf.service;
+
+
+import com.ccarlosf.pojo.Items;
+import com.ccarlosf.pojo.ItemsImg;
+import com.ccarlosf.pojo.ItemsParam;
+import com.ccarlosf.pojo.ItemsSpec;
+
+import java.util.List;
+
+public interface ItemService {
+
+    /**
+     * 根据商品ID查询详情
+     * @param itemId
+     * @return
+     */
+    public Items queryItemById(String itemId);
+
+    /**
+     * 根据商品id查询商品图片列表
+     * @param itemId
+     * @return
+     */
+    public List<ItemsImg> queryItemImgList(String itemId);
+
+    /**
+     * 根据商品id查询商品规格
+     * @param itemId
+     * @return
+     */
+    public List<ItemsSpec> queryItemSpecList(String itemId);
+
+    /**
+     * 根据商品id查询商品参数
+     * @param itemId
+     * @return
+     */
+    public ItemsParam queryItemParam(String itemId);
+
+}
---
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-service/src/main/java/com/ccarlosf/service/impl/ItemServiceImpl.java
@@ -0,0 +1,68 @@
+package com.ccarlosf.service.impl;
+
+import com.ccarlosf.mapper.ItemsImgMapper;
+import com.ccarlosf.mapper.ItemsMapper;
+import com.ccarlosf.mapper.ItemsParamMapper;
+import com.ccarlosf.mapper.ItemsSpecMapper;
+import com.ccarlosf.pojo.Items;
+import com.ccarlosf.pojo.ItemsImg;
+import com.ccarlosf.pojo.ItemsParam;
+import com.ccarlosf.pojo.ItemsSpec;
+import com.ccarlosf.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.*;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private ItemsMapper itemsMapper;
+    @Autowired
+    private ItemsImgMapper itemsImgMapper;
+    @Autowired
+    private ItemsSpecMapper itemsSpecMapper;
+    @Autowired
+    private ItemsParamMapper itemsParamMapper;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Items queryItemById(String itemId) {
+        return itemsMapper.selectByPrimaryKey(itemId);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<ItemsImg> queryItemImgList(String itemId) {
+        Example itemsImgExp = new Example(ItemsImg.class);
+        Example.Criteria criteria = itemsImgExp.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+
+        return itemsImgMapper.selectByExample(itemsImgExp);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<ItemsSpec> queryItemSpecList(String itemId) {
+        Example itemsSpecExp = new Example(ItemsSpec.class);
+        Example.Criteria criteria = itemsSpecExp.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+
+        return itemsSpecMapper.selectByExample(itemsSpecExp);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public ItemsParam queryItemParam(String itemId) {
+        Example itemsParamExp = new Example(ItemsParam.class);
+        Example.Criteria criteria = itemsParamExp.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+
+        return itemsParamMapper.selectOneByExample(itemsParamExp);
+    }
+
+}
