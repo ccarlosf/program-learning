76bba8e5d126541be59a9339fe03c41e7ef14d84
ccarlos <2238513100@qq.com>
2021-01-03 19:39:52 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第3周   地址，订单，支付，定时任务开发 第1章 收货地址功能开发 1-3 收货地址 - 查询收货地址列表

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/AddressController.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/AddressController.java
@@ -1,8 +1,15 @@
 package com.ccarlosf.controller;
 
+import com.ccarlosf.pojo.UserAddress;
+import com.ccarlosf.service.AddressService;
+import com.ccarlosf.utils.JSONResult;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(value = "地址相关", tags = {"地址相关的api接口"})
 @RequestMapping("address")
@@ -18,5 +25,20 @@ public class AddressController {
      * 5. 设置默认地址
      */
 
+    @Autowired
+    private AddressService addressService;
+
+    @ApiOperation(value = "根据用户id查询收货地址列表", notes = "根据用户id查询收货地址列表", httpMethod = "POST")
+    @PostMapping("/list")
+    public JSONResult list(
+            @RequestParam String userId) {
+
+        if (StringUtils.isBlank(userId)) {
+            return JSONResult.errorMsg("");
+        }
+
+        List<UserAddress> list = addressService.queryAll(userId);
+        return JSONResult.ok(list);
+    }
 
 }
---
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-service/src/main/java/com/ccarlosf/service/impl/AddressServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ccarlosf.service.impl;
+
+import com.ccarlosf.mapper.UserAddressMapper;
+import com.ccarlosf.pojo.UserAddress;
+import com.ccarlosf.service.AddressService;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    @Autowired
+    private UserAddressMapper userAddressMapper;
+
+    @Autowired
+    private Sid sid;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<UserAddress> queryAll(String userId) {
+
+        UserAddress ua = new UserAddress();
+        ua.setUserId(userId);
+
+        return userAddressMapper.select(ua);
+    }
+
+}
