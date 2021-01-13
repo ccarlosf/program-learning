0273322382e07764aa8961acf8319ee00271fe0c
ccarlos <2238513100@qq.com>
2021-01-13 21:55:54 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第3周   地址，订单，支付，定时任务开发 第3章 创建订单 3-4 创建订单 - 测试订单创建与回滚

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/OrdersController.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/OrdersController.java
@@ -2,11 +2,13 @@ package com.ccarlosf.controller;
 
 import com.ccarlosf.enums.PayMethod;
 import com.ccarlosf.pojo.bo.SubmitOrderBO;
+import com.ccarlosf.service.OrderService;
 import com.ccarlosf.utils.JSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,9 @@ import javax.servlet.http.HttpServletResponse;
 public class OrdersController extends BaseController {
 
     final static Logger logger = LoggerFactory.getLogger(OrdersController.class);
+
+    @Autowired
+    private OrderService orderService;
 
 
     @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
@@ -37,6 +42,13 @@ public class OrdersController extends BaseController {
         }
 
         System.out.println(submitOrderBO.toString());
+
+        // 1. 创建订单
+        orderService.createOrder(submitOrderBO);
+
+        // 2. 创建订单以后，移除购物车中已结算（已提交）的商品
+        // 3. 向支付中心发送当前订单，用于保存支付中心的订单数据
+
         return JSONResult.ok();
     }
 }
