b7124bde8e1e28d91816abb4993254062d85868b
ccarlos <2238513100@qq.com>
2021-01-17 22:31:19 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第3周   地址，订单，支付，定时任务开发 第6章 定时任务功能开发 6-1 定时任务 - 构建定时任务task

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/FoodieApplication.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/FoodieApplication.java
@@ -4,6 +4,7 @@ package com.ccarlosf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
@@ -11,6 +12,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "com.ccarlosf.mapper")
 // 扫描所有包以及相关组件包
 @ComponentScan(basePackages = {"com.ccarlosf","org.n3r.idworker"})
+//@EnableTransactionManagement
+@EnableScheduling       // 开启定时任务
 public class FoodieApplication {
 
     public static void main(String[] args) {
---
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/config/OrderJob.java
@@ -0,0 +1,23 @@
+package com.ccarlosf.config;
+
+import com.ccarlosf.service.OrderService;
+import com.ccarlosf.utils.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderJob {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Scheduled(cron = "0/3 * * * * ?")
+//    @Scheduled(cron = "0 0 0/1 * * ?")
+    public void autoCloseOrder() {
+//        orderService.closeOrder();
+        System.out.println("执行定时任务，当前时间为："
+                + DateUtil.getCurrentDateString(DateUtil.DATETIME_PATTERN));
+    }
+
+}
