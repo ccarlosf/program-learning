e05efb6ee3c5cc0b1e30213828fc6165dbd22b2a
ccarlos <2238513100@qq.com>
2021-01-24 10:20:28 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第4周   用户中心 ,订单/评价管理开发 第2章 用户中心 -上传头像功能开发 2-1 上传头像 - 定义文件保存位置（10:44）

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/BaseController.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/BaseController.java
@@ -2,6 +2,8 @@ package com.ccarlosf.controller;
 
 import org.springframework.stereotype.Controller;
 
+import java.io.File;
+
 @Controller
 public class BaseController {
 
@@ -23,6 +25,15 @@ public class BaseController {
     // 微信支付成功 -> 支付中心 -> 天天吃货平台
     //                       |-> 回调通知的url
 //    String payReturnUrl = "http://api.z.mukewang.com/foodie-dev-api/orders/notifyMerchantOrderPaid";
+
+
+    // 用户上传头像的位置
+    public static final String IMAGE_USER_FACE_LOCATION = File.separator + "workspaces" +
+            File.separator + "images" +
+            File.separator + "foodie" +
+            File.separator + "faces";
+//    public static final String IMAGE_USER_FACE_LOCATION = "/workspaces/images/foodie/faces";
+
 
 
 }
--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/center/CenterUserController.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/center/CenterUserController.java
@@ -15,10 +15,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,7 +32,34 @@ public class CenterUserController extends BaseController {
 
     @Autowired
     private CenterUserService centerUserService;
-    
+
+    @ApiOperation(value = "用户头像修改", notes = "用户头像修改", httpMethod = "POST")
+    @PostMapping("uploadFace")
+    public JSONResult uploadFace(
+            @ApiParam(name = "userId", value = "用户id", required = true)
+            @RequestParam String userId,
+            @ApiParam(name = "file", value = "用户头像", required = true)
+                    MultipartFile file,
+            HttpServletRequest request, HttpServletResponse response) {
+
+        // .sh .php
+
+        // 定义头像保存的地址
+        String fileSpace = IMAGE_USER_FACE_LOCATION;
+        // 在路径上为每一个用户增加一个userid，用于区分不同用户上传
+        String uploadPathPrefix = File.separator + userId;
+
+        // 开始文件上传
+        if (file != null) {
+
+        } else {
+            return JSONResult.errorMsg("文件不能为空！");
+        }
+
+        return JSONResult.ok();
+    }
+
+
     @ApiOperation(value = "修改用户信息", notes = "修改用户信息", httpMethod = "POST")
     @PostMapping("update")
     public JSONResult update(
