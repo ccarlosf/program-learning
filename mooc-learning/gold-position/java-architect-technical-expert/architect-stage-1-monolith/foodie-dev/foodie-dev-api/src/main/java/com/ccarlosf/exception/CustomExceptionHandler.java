a2e861c2a62114d6782e0629b878061dfdbf248f
ccarlos <2238513100@qq.com>
2021-01-24 11:22:26 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第4周   用户中心 ,订单/评价管理开发 第2章 用户中心 -上传头像功能开发 2-7 上传头像-大小限制，以及自定义捕获异常（08:58）

---
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/exception/CustomExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.ccarlosf.exception;
+
+import com.ccarlosf.utils.JSONResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    // 上传文件超过500k，捕获异常：MaxUploadSizeExceededException
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public JSONResult handlerMaxUploadFile(MaxUploadSizeExceededException ex) {
+        return JSONResult.errorMsg("文件上传大小不能超过500k，请压缩图片或者降低图片质量再上传！");
+    }
+}
