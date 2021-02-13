bed22215f8e35cb28189bc00162cd283f057f2ca
ccarlos <2238513100@qq.com>
2021-02-13 11:30:34 +0800

    阶段一：单体电商项目架构，开发与上线（1~5周） 第5周   云服务器部署上线 第1章 云服务器部署上线 1-18 解决Cookie异常，测试订单支付流程（10:38）

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-shop/js/app.js
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-shop/js/app.js
@@ -7,7 +7,7 @@ window.app = {
     // cookieDomain: "", // cookie 域
 
     /* 生产环境 */
-     serverUrl: "http://api.z.mukewang.com:8088/foodie-dev-api",                      // 接口服务接口地址
+    serverUrl: "http://api.z.mukewang.com:8088/foodie-dev-api",                      // 接口服务接口地址
     paymentServerUrl: "http://payment.t.mukewang.com/foodie-payment", // 支付中心服务地址
     shopServerUrl: "http://shop.z.mukewang.com:8080/foodie-shop/",                            // 门户网站地址
     centerServerUrl: "http://center.z.mukewang.com:8080/foodie-center/",                        // 用户中心地址
