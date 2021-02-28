78443e0bfc41d8982f4df600cbc9a4444ecc987a
ccarlos <2238513100@qq.com>
2021-02-28 16:48:00 +0800

    阶段二：从单体到高可用集群演进（6-8周） 第6周   LVS+Nginx实现高可用集群 第2章 Nginx进阶与实战 2-27 Nginx控制浏览器缓存 - 1（08:46）

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/FoodieApplication.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/FoodieApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-// 扫描 mybatis 通用 mapper 所在的包
+// 扫描 mybatis 通用 mapper 所在的包目录
 @MapperScan(basePackages = "com.ccarlosf.mapper")
 // 扫描所有包以及相关组件包
 @ComponentScan(basePackages = {"com.ccarlosf","org.n3r.idworker"})
