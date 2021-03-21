a001b95a0c3a7f24f726dd20f442ff874d6caa3b
ccarlos <2238513100@qq.com>
2021-03-21 16:07:12 +0800

    阶段二：从单体到高可用集群演进（6-8周） 第7周   主从复制高可用Redis集群 第2章 SpringBoot整合Redis实战 2-4 Redis 操作工具类讲解 （08:31）

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/RedisController.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/RedisController.java
@@ -1,11 +1,16 @@
 package com.ccarlosf.controller;
 
+import com.ccarlosf.utils.RedisOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @ApiIgnore
 @RestController
@@ -15,20 +20,26 @@ public class RedisController {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @Autowired
+    private RedisOperator redisOperator;
+
     @GetMapping("/set")
     public Object set(String key, String value) {
-        redisTemplate.opsForValue().set(key, value);
+//        redisTemplate.opsForValue().set(key, value);
+        redisOperator.set(key, value);
         return "OK";
     }
 
     @GetMapping("/get")
     public String get(String key) {
-        return (String)redisTemplate.opsForValue().get(key);
+//        return (String)redisTemplate.opsForValue().get(key);
+        return redisOperator.get(key);
     }
 
     @GetMapping("/delete")
     public Object delete(String key) {
-        redisTemplate.delete(key);
+//        redisTemplate.delete(key);
+        redisOperator.del(key);
         return "OK";
     }
 
