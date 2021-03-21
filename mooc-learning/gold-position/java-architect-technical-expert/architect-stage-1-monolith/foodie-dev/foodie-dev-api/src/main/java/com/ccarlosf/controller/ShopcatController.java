e5354e0fb318d54c6d83d5e606efca16f8aa8534
ccarlos <2238513100@qq.com>
2021-03-21 16:48:27 +0800

    阶段二：从单体到高可用集群演进（6-8周） 第7周   主从复制高可用Redis集群 第2章 SpringBoot整合Redis实战 2-6 基于Redis优化购物车 - 添加商品（09:25）

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/ShopcatController.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-1-monolith/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/ShopcatController.java
@@ -2,18 +2,26 @@ package com.ccarlosf.controller;
 
 import com.ccarlosf.pojo.bo.ShopcartBO;
 import com.ccarlosf.utils.JSONResult;
+import com.ccarlosf.utils.JsonUtils;
+import com.ccarlosf.utils.RedisOperator;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Api(value = "购物车接口controller", tags = {"购物车接口相关的api"})
 @RequestMapping("shopcart")
 @RestController
-public class ShopcatController {
+public class ShopcatController extends BaseController {
+
+    @Autowired
+    private RedisOperator redisOperator;
 
     @ApiOperation(value = "添加商品到购物车", notes = "添加商品到购物车", httpMethod = "POST")
     @PostMapping("/add")
@@ -30,7 +38,34 @@ public class ShopcatController {
 
         System.out.println(shopcartBO);
 
-        // TODO 前端用户在登录的情况下，添加商品到购物车，会同时在后端同步购物车到redis缓存
+        // 前端用户在登录的情况下，添加商品到购物车，会同时在后端同步购物车到redis缓存
+        // 需要判断当前购物车中包含已经存在的商品，如果存在则累加购买数量
+        String shopcartJson = redisOperator.get(FOODIE_SHOPCART + ":" + userId);
+        List<ShopcartBO> shopcartList = null;
+        if (StringUtils.isNotBlank(shopcartJson)) {
+            // redis中已经有购物车了
+            shopcartList = JsonUtils.jsonToList(shopcartJson, ShopcartBO.class);
+            // 判断购物车中是否存在已有商品，如果有的话counts累加
+            boolean isHaving = false;
+            for (ShopcartBO sc : shopcartList) {
+                String tmpSpecId = sc.getSpecId();
+                if (tmpSpecId.equals(shopcartBO.getSpecId())) {
+                    sc.setBuyCounts(sc.getBuyCounts() + shopcartBO.getBuyCounts());
+                    isHaving = true;
+                }
+            }
+            if (!isHaving) {
+                shopcartList.add(shopcartBO);
+            }
+        } else {
+            // redis中没有购物车
+            shopcartList = new ArrayList<>();
+            // 直接添加到购物车中
+            shopcartList.add(shopcartBO);
+        }
+
+        // 覆盖现有redis中的购物车
+        redisOperator.set(FOODIE_SHOPCART + ":" + userId, JsonUtils.objectToJson(shopcartList));
 
         return JSONResult.ok();
     }
