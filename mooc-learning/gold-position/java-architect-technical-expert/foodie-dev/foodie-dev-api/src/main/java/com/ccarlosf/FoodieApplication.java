49366296fc7be017fbbb32c2630b4346d2c0109c
ccarlos <2238513100@qq.com>
2021-03-28 21:49:42 +0800

    阶段三：分布式架构-逐个击破分布式核心问题（9-17周） 第9周 分布式会话与单点登录SSO 第1章 Redis实现用户会话 1-7 SpringSession实现用户会话（12:42）

--- mooc-learning/gold-position/java-architect-technical-expert/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/FoodieApplication.java
+++ mooc-learning/gold-position/java-architect-technical-expert/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/FoodieApplication.java
@@ -3,17 +3,21 @@ package com.ccarlosf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+// 跳转security的拦截登录
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // 扫描 mybatis 通用 mapper 所在的包目录
 @MapperScan(basePackages = "com.ccarlosf.mapper")
 // 扫描所有包以及相关组件包
-@ComponentScan(basePackages = {"com.ccarlosf","org.n3r.idworker"})
+@ComponentScan(basePackages = {"com.ccarlosf", "org.n3r.idworker"})
 //@EnableTransactionManagement
 @EnableScheduling       // 开启定时任务
+@EnableRedisHttpSession // 开启使用redis作为spring session
 public class FoodieApplication {
 
     public static void main(String[] args) {
