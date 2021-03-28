6f5dcae484dff8a3bdb33d7f5677412c5855874b
ccarlos <2238513100@qq.com>
2021-03-28 22:11:25 +0800

    阶段三：分布式架构-逐个击破分布式核心问题（9-17周） 第9周 分布式会话与单点登录SSO 第2章 分布式会话拦截器 2-1 分布式会话拦截器1 - 构建拦截器（10:16）

---
+++ mooc-learning/gold-position/java-architect-technical-expert/foodie-dev/foodie-dev-api/src/main/java/com/ccarlosf/controller/interceptor/UserTokenInterceptor.java
@@ -0,0 +1,57 @@
+package com.ccarlosf.controller.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserTokenInterceptor implements HandlerInterceptor {
+
+    /**
+     * 拦截请求，在访问controller调用之前
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        System.out.println("进入到拦截器，被拦截。。。");
+
+        /**
+         * false: 请求被拦截，被驳回，验证出现问题
+         * true: 请求在经过验证校验以后，是OK的，是可以放行的
+         */
+        return false;
+    }
+
+
+    /**
+     * 请求访问controller之后，渲染视图之前
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    /**
+     * 请求访问controller之后，渲染视图之后
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
