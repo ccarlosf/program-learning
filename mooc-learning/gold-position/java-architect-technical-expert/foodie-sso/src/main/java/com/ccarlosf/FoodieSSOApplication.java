8cff0f3985f82ca6df37de110d299ffcf00f0ae1
ccarlos <2238513100@qq.com>
2021-03-30 21:31:46 +0800

    阶段三：分布式架构-逐个击破分布式核心问题（9-17周） 第9周 分布式会话与单点登录SSO 第3章 CAS单点登录 3-5 CAS单点登录 - 跳转至登录（08:02）

--- mooc-learning/gold-position/java-architect-technical-expert/foodie-sso/src/main/java/com/ccarlosf/FoodieSSOApplication.java
+++ mooc-learning/gold-position/java-architect-technical-expert/foodie-sso/src/main/java/com/ccarlosf/FoodieSSOApplication.java
@@ -2,11 +2,12 @@ package com.ccarlosf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
 // 跳转security的拦截登录
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // 扫描 mybatis 通用 mapper 所在的包目录
 @MapperScan(basePackages = "com.ccarlosf.mapper")
 // 扫描所有包以及相关组件包
--- mooc-learning/gold-position/java-architect-technical-expert/foodie-sso/src/main/java/com/ccarlosf/controller/SSOController.java
+++ mooc-learning/gold-position/java-architect-technical-expert/foodie-sso/src/main/java/com/ccarlosf/controller/SSOController.java
@@ -1,15 +1,26 @@
 package com.ccarlosf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class SSOController {
 
-    @ResponseBody
     @GetMapping("/login")
-    public String login() {
-        return "Hello World~";
+    public String login(String returnUrl,
+                        Model model,
+                        HttpServletRequest request,
+                        HttpServletResponse response) {
+
+        model.addAttribute("returnUrl", returnUrl);
+
+        // TODO 后续完善校验是否登录
+
+        // 用户从未登录过，第一次进入则跳转到CAS的统一登录页面
+        return "login";
     }
 }
