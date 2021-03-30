e92ac5bd68b1a86e897a06e5618fc48772b7bb81
ccarlos <2238513100@qq.com>
2021-03-30 22:10:05 +0800

    阶段三：分布式架构-逐个击破分布式核心问题（9-17周） 第9周 分布式会话与单点登录SSO 第3章 CAS单点登录 3-6 CAS单点登录 - 用户登录与校验（08:43）

--- mooc-learning/gold-position/java-architect-technical-expert/foodie-sso/src/main/java/com/ccarlosf/controller/SSOController.java
+++ mooc-learning/gold-position/java-architect-technical-expert/foodie-sso/src/main/java/com/ccarlosf/controller/SSOController.java
@@ -1,14 +1,33 @@
 package com.ccarlosf.controller;
 
+import com.ccarlosf.pojo.Users;
+import com.ccarlosf.pojo.vo.UsersVO;
+import com.ccarlosf.service.UserService;
+import com.ccarlosf.utils.JsonUtils;
+import com.ccarlosf.utils.MD5Utils;
+import com.ccarlosf.utils.RedisOperator;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class SSOController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RedisOperator redisOperator;
+
+    public static final String REDIS_USER_TOKEN = "redis_user_token";
 
     @GetMapping("/login")
     public String login(String returnUrl,
@@ -21,6 +40,42 @@ public class SSOController {
         // TODO 后续完善校验是否登录
 
         // 用户从未登录过，第一次进入则跳转到CAS的统一登录页面
+        return "login";
+    }
+
+    @PostMapping("/doLogin")
+    public String doLogin(String username,
+                          String password,
+                          String returnUrl,
+                          Model model,
+                          HttpServletRequest request,
+                          HttpServletResponse response) throws Exception {
+
+        model.addAttribute("returnUrl", returnUrl);
+
+        // 0. 判断用户名和密码必须不为空
+        if (StringUtils.isBlank(username) ||
+                StringUtils.isBlank(password)) {
+            model.addAttribute("errmsg", "用户名或密码不能为空");
+            return "login";
+        }
+
+        // 1. 实现登录
+        Users userResult = userService.queryUserForLogin(username,
+                MD5Utils.getMD5Str(password));
+        if (userResult == null) {
+            model.addAttribute("errmsg", "用户名或密码不正确");
+            return "login";
+        }
+
+        // 2. 实现用户的redis会话
+        String uniqueToken = UUID.randomUUID().toString().trim();
+        UsersVO usersVO = new UsersVO();
+        BeanUtils.copyProperties(userResult, usersVO);
+        usersVO.setUserUniqueToken(uniqueToken);
+        redisOperator.set(REDIS_USER_TOKEN + ":" + userResult.getId(),
+                JsonUtils.objectToJson(usersVO));
+
         return "login";
     }
 }
