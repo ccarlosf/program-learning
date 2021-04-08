a82f3ee997a8ea50355f60ba40decb1f69176584
ccarlos <2238513100@qq.com>
2021-04-08 22:35:03 +0800

    阶段三：分布式架构-逐个击破分布式核心问题（9-17周） 第10周 分布式搜索引擎-ES 第6章 Elasticsearch整合SpringBoot 6-1 Elasticsearch整合SpringBoot - 配置文件（16分钟）

---
+++ mooc-learning/gold-position/java-architect-technical-expert/foodie-dev/foodie-dev-search/src/main/java/com/ccarlosf/FoodieSearchApplication.java
@@ -0,0 +1,13 @@
+package com.ccarlosf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodieSearchApplication.class, args);
+    }
+
+}
