d5f6ba9f86018664d1b502c2610a425ced266e0e
ccarlos <2238513100@qq.com>
2021-04-09 21:47:37 +0800

    阶段三：分布式架构-逐个击破分布式核心问题（9-17周） 第10周 分布式搜索引擎-ES 第6章 Elasticsearch整合SpringBoot 6-4 Elasticsearch整合SpringBoot - 索引的删除与mappings更新（10:15）

--- mooc-learning/gold-position/java-architect-technical-expert/foodie-dev/foodie-dev-search/src/test/java/com/test/ESTest.java
+++ mooc-learning/gold-position/java-architect-technical-expert/foodie-dev/foodie-dev-search/src/test/java/com/test/ESTest.java
@@ -18,6 +18,15 @@ public class ESTest {
     @Autowired
     private ElasticsearchTemplate esTemplate;
 
+    /**
+     * 不建议使用 ElasticsearchTemplate 对索引进行管理（创建索引，更新映射，删除索引）
+     * 索引就像是数据库或者数据库中的表，我们平时是不会是通过java代码频繁的去创建修改删除数据库或者表的
+     * 我们只会针对数据做CRUD的操作
+     * 在es中也是同理，我们尽量使用 ElasticsearchTemplate 对文档数据做CRUD的操作
+     * 1. 属性（FieldType）类型不灵活
+     * 2. 主分片与副本分片数无法设置
+     */
+
     @Test
     public void createIndexStu() {
 
@@ -25,8 +34,17 @@ public class ESTest {
         stu.setStuId(1002L);
         stu.setName("spider man");
         stu.setAge(22);
+        stu.setMoney(18.8f);
+        stu.setSign("I am spider man");
+        stu.setDescription("I wish i am a spider man");
 
         IndexQuery indexQuery = new IndexQueryBuilder().withObject(stu).build();
         esTemplate.index(indexQuery);
     }
+
+    @Test
+    public void deleteIndexStu() {
+        esTemplate.deleteIndex(Stu.class);
+    }
+
 }
