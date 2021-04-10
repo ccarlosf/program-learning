8b55dfa9e0ef2ce4c82579e8255426f3cd6ea720
ccarlos <2238513100@qq.com>
2021-04-10 09:44:40 +0800

    阶段三：分布式架构-逐个击破分布式核心问题（9-17周） 第10周 分布式搜索引擎-ES 第6章 Elasticsearch整合SpringBoot 6-8 Elasticsearch整合SpringBoot - 实现高亮（18:38）

--- mooc-learning/gold-position/java-architect-technical-expert/foodie-dev/foodie-dev-search/src/test/java/com/test/ESTest.java
+++ mooc-learning/gold-position/java-architect-technical-expert/foodie-dev/foodie-dev-search/src/test/java/com/test/ESTest.java
@@ -3,7 +3,15 @@ package com.test;
 import com.ccarlosf.FoodieSearchApplication;
 import com.ccarlosf.es.pojo.Stu;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.FieldSortBuilder;
+import org.elasticsearch.search.sort.SortBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +19,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchResultMapper;
 import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
 import org.springframework.data.elasticsearch.core.query.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -106,6 +117,70 @@ public class ESTest {
                 .withPageable(pageable)
                 .build();
         AggregatedPage<Stu> pagedStu = esTemplate.queryForPage(query, Stu.class);
+        System.out.println("检索后的总分页数目为：" + pagedStu.getTotalPages());
+        List<Stu> stuList = pagedStu.getContent();
+        for (Stu s : stuList) {
+            System.out.println(s);
+        }
+    }
+
+    @Test
+    public void highlightStuDoc() {
+
+        String preTag = "<font color='red'>";
+        String postTag = "</font>";
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        SortBuilder sortBuilder = new FieldSortBuilder("money")
+                .order(SortOrder.DESC);
+        SortBuilder sortBuilderAge = new FieldSortBuilder("age")
+                .order(SortOrder.ASC);
+
+        SearchQuery query = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.matchQuery("description", "save man"))
+                .withHighlightFields(new HighlightBuilder.Field("description")
+                        .preTags(preTag)
+                        .postTags(postTag))
+                .withSort(sortBuilder)
+                .withSort(sortBuilderAge)
+                .withPageable(pageable)
+                .build();
+        AggregatedPage<Stu> pagedStu = esTemplate.queryForPage(query, Stu.class, new SearchResultMapper() {
+            @Override
+            public <T> AggregatedPage<T> mapResults(SearchResponse response, Class<T> clazz, Pageable pageable) {
+
+                List<Stu> stuListHighlight = new ArrayList<>();
+
+                SearchHits hits = response.getHits();
+                for (SearchHit h : hits) {
+                    HighlightField highlightField = h.getHighlightFields().get("description");
+                    String description = highlightField.getFragments()[0].toString();
+
+                    Object stuId = (Object)h.getSourceAsMap().get("stuId");
+                    String name = (String)h.getSourceAsMap().get("name");
+                    Integer age = (Integer)h.getSourceAsMap().get("age");
+                    String sign = (String)h.getSourceAsMap().get("sign");
+                    Object money = (Object)h.getSourceAsMap().get("money");
+
+                    Stu stuHL = new Stu();
+                    stuHL.setDescription(description);
+                    stuHL.setStuId(Long.valueOf(stuId.toString()));
+                    stuHL.setName(name);
+                    stuHL.setAge(age);
+                    stuHL.setSign(sign);
+                    stuHL.setMoney(Float.valueOf(money.toString()));
+
+                    stuListHighlight.add(stuHL);
+                }
+
+                if (stuListHighlight.size() > 0) {
+                    return new AggregatedPageImpl<>((List<T>)stuListHighlight);
+                }
+
+                return null;
+            }
+        });
         System.out.println("检索后的总分页数目为：" + pagedStu.getTotalPages());
         List<Stu> stuList = pagedStu.getContent();
         for (Stu s : stuList) {
