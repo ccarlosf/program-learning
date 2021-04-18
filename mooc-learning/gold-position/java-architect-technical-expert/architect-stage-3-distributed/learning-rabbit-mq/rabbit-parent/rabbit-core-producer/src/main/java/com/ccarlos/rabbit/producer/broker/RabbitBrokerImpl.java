d444436f4a258713f4191a7d08703b08251bd19b
ccarlos <2238513100@qq.com>
2021-04-18 16:37:26 +0800

    阶段三：分布式架构-逐个击破分布式核心问题（9-17周） 第12周   分布式消息队列-RabbitMQ 第3章 RabbitMQ基础组件封装实战 3-9 确认消息实现（03:28）

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-3-distributed/learning-rabbit-mq/rabbit-parent/rabbit-core-producer/src/main/java/com/ccarlos/rabbit/producer/broker/RabbitBrokerImpl.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-3-distributed/learning-rabbit-mq/rabbit-parent/rabbit-core-producer/src/main/java/com/ccarlos/rabbit/producer/broker/RabbitBrokerImpl.java
@@ -63,6 +63,8 @@ public class RabbitBrokerImpl implements RabbitBroker {
 
     @Override
     public void confirmSend(Message message) {
+        message.setMessageType(MessageType.CONFIRM);
+        sendKernel(message);
     }
 
     @Override
