94490f7b365b95d5b5e8da389215a996035f65e4
ccarlos <2238513100@qq.com>
2021-05-05 09:15:19 +0800

    阶段三：分布式架构-逐个击破分布式核心问题（9-17周） 第12周   分布式消息队列-RabbitMQ 第3章 RabbitMQ基础组件封装实战 3-17 定时任务通用组件封装-1（22:02）

---
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-3-distributed/learning-rabbit-mq/rabbit-parent/rabbit-task/src/main/java/com/ccarlos/rabbit/task/autoconfigure/JobParserAutoConfiguration.java
@@ -0,0 +1,30 @@
+package com.ccarlos.rabbit.task.autoconfigure;
+
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@ConditionalOnProperty(prefix = "elastic.job.zk", name = {"namespace", "serverLists"}, matchIfMissing = false)
+@EnableConfigurationProperties(JobZookeeperProperties.class)
+public class JobParserAutoConfiguration {
+
+	@Bean(initMethod = "init")
+	public ZookeeperRegistryCenter zookeeperRegistryCenter(JobZookeeperProperties jobZookeeperProperties) {
+		ZookeeperConfiguration zkConfig = new ZookeeperConfiguration(jobZookeeperProperties.getServerLists(),
+				jobZookeeperProperties.getNamespace());
+		zkConfig.setBaseSleepTimeMilliseconds(jobZookeeperProperties.getBaseSleepTimeMilliseconds());
+		zkConfig.setMaxSleepTimeMilliseconds(jobZookeeperProperties.getMaxSleepTimeMilliseconds());
+		zkConfig.setConnectionTimeoutMilliseconds(jobZookeeperProperties.getConnectionTimeoutMilliseconds());
+		zkConfig.setSessionTimeoutMilliseconds(jobZookeeperProperties.getSessionTimeoutMilliseconds());
+		zkConfig.setMaxRetries(jobZookeeperProperties.getMaxRetries());
+		zkConfig.setDigest(jobZookeeperProperties.getDigest());
+		log.info("初始化job注册中心配置成功, zkaddress : {}, namespace : {}", jobZookeeperProperties.getServerLists(), jobZookeeperProperties.getNamespace());
+		return new ZookeeperRegistryCenter(zkConfig);
+	}
+}
