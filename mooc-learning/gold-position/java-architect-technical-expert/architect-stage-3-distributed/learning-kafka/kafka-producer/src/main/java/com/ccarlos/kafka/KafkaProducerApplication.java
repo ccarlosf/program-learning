37895e26f73660719b2a7604e26e075398e3cebc
ccarlos <2238513100@qq.com>
2021-05-28 21:20:37 +0800

    阶段三：分布式架构-逐个击破分布式核心问题（9-17周） 第13周   分布式消息队列-Kafka 1-4 Kafka与springboot整合_生产者讲解

---
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-3-distributed/learning-kafka/kafka-producer/src/main/java/com/ccarlos/kafka/KafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.ccarlos.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerApplication.class, args);
+
+    }
+}
