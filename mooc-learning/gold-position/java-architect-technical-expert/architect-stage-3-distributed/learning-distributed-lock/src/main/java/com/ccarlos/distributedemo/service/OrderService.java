3ba3af6272a5c0c4e539f31d95041ae8ef8850d3
ccarlos <2238513100@qq.com>
2021-06-03 21:48:46 +0800

    阶段三：分布式架构-逐个击破分布式核心问题（9-17周） 第14周   分布式锁 第2章 分布式锁设计 2-4 基于Synchronized锁解决超卖问题（方法锁）（10:52）

--- mooc-learning/gold-position/java-architect-technical-expert/architect-stage-3-distributed/learning-distributed-lock/src/main/java/com/ccarlos/distributedemo/service/OrderService.java
+++ mooc-learning/gold-position/java-architect-technical-expert/architect-stage-3-distributed/learning-distributed-lock/src/main/java/com/ccarlos/distributedemo/service/OrderService.java
@@ -7,7 +7,11 @@ import com.ccarlos.distributedemo.model.Order;
 import com.ccarlos.distributedemo.model.OrderItem;
 import com.ccarlos.distributedemo.model.Product;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
@@ -28,8 +32,12 @@ public class OrderService {
     private int purchaseProductId = 100100;
     //购买商品数量
     private int purchaseProductNum = 1;
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+    @Autowired
+    private TransactionDefinition transactionDefinition;
 
-
+    // 程序判读，商品超卖
     /*@Transactional(rollbackFor = Exception.class)
     public Integer createOrder() throws Exception {
         Product product = productMapper.selectByPrimaryKey(purchaseProductId);
@@ -76,6 +84,7 @@ public class OrderService {
         return order.getId();
     }*/
 
+    // update行锁，商品超卖
    /* @Transactional(rollbackFor = Exception.class)
     public Integer createOrder() throws Exception {
         Product product = productMapper.selectByPrimaryKey(purchaseProductId);
@@ -126,8 +135,13 @@ public class OrderService {
         return order.getId();
     }*/
 
-    @Transactional(rollbackFor = Exception.class)
-    public Integer createOrder() throws Exception {
+    // synchronized 商品超卖
+    // synchronized 加在方法上，还是不起作用，原因：前一个线程事务还没提交，
+    // 第二个线程就开始读取数据，解决办法，手动控制事务，锁释放时，也要保证事务提交
+ /*   @Transactional(rollbackFor = Exception.class)
+    public synchronized Integer createOrder() throws Exception {
+        // synchronized 加在方法上，还是不起作用，原因：前一个线程事务还没提交，
+        // 第二个线程就开始读取数据，解决办法，手动控制事务，锁释放时，也要保证事务提交
         Product product = productMapper.selectByPrimaryKey(purchaseProductId);
         if (product == null) {
             throw new Exception("购买商品：" + purchaseProductId + "不存在");
@@ -176,6 +190,64 @@ public class OrderService {
         orderItem.setUpdateTime(LocalDateTime.now());
         orderItem.setUpdateUser("xxx");
         orderItemMapper.insertSelective(orderItem);
+        return order.getId();
+    }*/
+
+    // 手动控制事务，锁释放时，也要保证事务提交
+//    @Transactional(rollbackFor = Exception.class)
+    public synchronized Integer createOrder() throws Exception {
+        TransactionStatus transaction = platformTransactionManager.getTransaction(transactionDefinition);
+        Product product = productMapper.selectByPrimaryKey(purchaseProductId);
+        if (product == null) {
+            platformTransactionManager.rollback(transaction);
+            throw new Exception("购买商品：" + purchaseProductId + "不存在");
+        }
+
+        //商品当前库存
+        Integer currentCount = product.getCount();
+        System.out.println(Thread.currentThread().getName() + "库存数：" + currentCount);
+        //校验库存
+        if (purchaseProductNum > currentCount) {
+            platformTransactionManager.rollback(transaction);
+            throw new Exception("商品" + purchaseProductId + "仅剩" + currentCount + "件，无法购买");
+        }
+        //计算剩余库存
+//        Integer leftCount = currentCount - purchaseProductNum;
+//        //更新库存
+//        product.setCount(leftCount);
+//        product.setUpdateTime(LocalDateTime.now());
+//        product.setUpdateUser("xxx");
+//        productMapper.updateByPrimaryKeySelective(product);
+
+
+        productMapper.updateProductCount(purchaseProductNum,"xxx",
+                LocalDateTime.now(),product.getId());
+
+        //检索商品的库存
+        //如果商品库存为负数，抛出异常
+
+        Order order = new Order();
+        order.setOrderAmount(product.getPrice().multiply(new BigDecimal(purchaseProductNum)));
+        order.setOrderStatus(1);//待处理
+        order.setReceiverName("xxx");
+        order.setReceiverMobile("13311112222");
+        order.setCreateTime(LocalDateTime.now());
+        order.setCreateUser("xxx");
+        order.setUpdateTime(LocalDateTime.now());
+        order.setUpdateUser("xxx");
+        orderMapper.insertSelective(order);
+
+        OrderItem orderItem = new OrderItem();
+        orderItem.setOrderId(order.getId());
+        orderItem.setProductId(product.getId());
+        orderItem.setPurchasePrice(product.getPrice());
+        orderItem.setPurchaseNum(purchaseProductNum);
+        orderItem.setCreateUser("xxx");
+        orderItem.setCreateTime(LocalDateTime.now());
+        orderItem.setUpdateTime(LocalDateTime.now());
+        orderItem.setUpdateUser("xxx");
+        orderItemMapper.insertSelective(orderItem);
+        platformTransactionManager.commit(transaction);
         return order.getId();
     }
     
